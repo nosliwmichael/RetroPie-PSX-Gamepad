ba19f36a6a4eef18fb8a2b94deec730f93d44472
nosliwmichael <nosliw.michael@gmail.com>
2024-01-29 00:28:30 -0600

    Fix btn mapping

--- Events.py
+++ Events.py
@@ -4,10 +4,10 @@ import uinput
 events = (
     uinput.ABS_X + (0, 1000, 0, 0),   # Left joystick X-axis
     uinput.ABS_Y + (0, 1000, 0, 0),   # Left joystick Y-axis
-    uinput.ABS_THUMBL,                # Left joystick button
+    uinput.BTN_THUMB,                # Left joystick button
     uinput.ABS_RX + (0, 1000, 0, 0),  # right joystick X-axis
     uinput.ABS_RY + (0, 1000, 0, 0),  # Right joystick Y-axis
-    uinput.ABS_THUMBR,                # Right joystick button
+    uinput.BTN_THUMBR,                # Right joystick button
     uinput.BTN_A,
     uinput.BTN_B,
     uinput.BTN_X,
@@ -23,12 +23,12 @@ def eventHandler(gamepad, values):
     # Left Joystick
     gamepad.emit(uinput.ABS_X, values[0], syn=False)
     gamepad.emit(uinput.ABS_Y, values[1], syn=False)
-    gamepad.emit(uinput.ABS_THUMBL, values[2], syn=False)
+    gamepad.emit(uinput.BTN_THUMB, values[2], syn=False)
 
     # Right Joystick
     gamepad.emit(uinput.ABS_RX, values[3], syn=False)
     gamepad.emit(uinput.ABS_RY, values[4], syn=False)
-    gamepad.emit(uinput.ABS_THUMBR, values[5], syn=False)
+    gamepad.emit(uinput.BTN_THUMBR, values[5], syn=False)
 
     # A, B, X, Y Buttons
     gamepad.emit(uinput.BTN_A, values[6], syn=False)
