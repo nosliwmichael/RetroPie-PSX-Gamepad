45553b46fed476366f91258f9a1ce4e85b6afa32
nosliwmichael <nosliw.michael@gmail.com>
2024-01-31 01:04:25 -0600

    Read configs as integer instead of string

--- ButtonMap.py
+++ ButtonMap.py
@@ -16,22 +16,22 @@ class ButtonMap:
         gamepad_configs.read(config_file_path)
 
         # GPIO
-        self.DPAD_UP = gamepad_configs.get(GPIO_SECTION, 'DPAD_UP')
-        self.DPAD_DOWN = gamepad_configs.get(GPIO_SECTION, 'DPAD_DOWN')
-        self.DPAD_LEFT = gamepad_configs.get(GPIO_SECTION, 'DPAD_LEFT')
-        self.DPAD_RIGHT = gamepad_configs.get(GPIO_SECTION, 'DPAD_RIGHT')
-        self.A = gamepad_configs.get(GPIO_SECTION, 'A')
-        self.B = gamepad_configs.get(GPIO_SECTION, 'B')
-        self.X = gamepad_configs.get(GPIO_SECTION, 'X')
-        self.Y = gamepad_configs.get(GPIO_SECTION, 'Y')
+        self.DPAD_UP = gamepad_configs.getint(GPIO_SECTION, 'DPAD_UP')
+        self.DPAD_DOWN = gamepad_configs.getint(GPIO_SECTION, 'DPAD_DOWN')
+        self.DPAD_LEFT = gamepad_configs.getint(GPIO_SECTION, 'DPAD_LEFT')
+        self.DPAD_RIGHT = gamepad_configs.getint(GPIO_SECTION, 'DPAD_RIGHT')
+        self.A = gamepad_configs.getint(GPIO_SECTION, 'A')
+        self.B = gamepad_configs.getint(GPIO_SECTION, 'B')
+        self.X = gamepad_configs.getint(GPIO_SECTION, 'X')
+        self.Y = gamepad_configs.getint(GPIO_SECTION, 'Y')
         
         # MCP3008
-        self.LEFT_JOYSTICK_BTN = gamepad_configs.get(MCP3008_SECTION, 'LEFT_JOYSTICK_BTN')
-        self.LEFT_JOYSTICK_X = gamepad_configs.get(MCP3008_SECTION, 'LEFT_JOYSTICK_X')
-        self.LEFT_JOYSTICK_Y = gamepad_configs.get(MCP3008_SECTION, 'LEFT_JOYSTICK_Y')
-        self.RIGHT_JOYSTICK_BTN = gamepad_configs.get(MCP3008_SECTION, 'RIGHT_JOYSTICK_BTN')
-        self.RIGHT_JOYSTICK_X = gamepad_configs.get(MCP3008_SECTION, 'RIGHT_JOYSTICK_X')
-        self.RIGHT_JOYSTICK_Y = gamepad_configs.get(MCP3008_SECTION, 'RIGHT_JOYSTICK_Y')
+        self.LEFT_JOYSTICK_BTN = gamepad_configs.getint(MCP3008_SECTION, 'LEFT_JOYSTICK_BTN')
+        self.LEFT_JOYSTICK_X = gamepad_configs.getint(MCP3008_SECTION, 'LEFT_JOYSTICK_X')
+        self.LEFT_JOYSTICK_Y = gamepad_configs.getint(MCP3008_SECTION, 'LEFT_JOYSTICK_Y')
+        self.RIGHT_JOYSTICK_BTN = gamepad_configs.getint(MCP3008_SECTION, 'RIGHT_JOYSTICK_BTN')
+        self.RIGHT_JOYSTICK_X = gamepad_configs.getint(MCP3008_SECTION, 'RIGHT_JOYSTICK_X')
+        self.RIGHT_JOYSTICK_Y = gamepad_configs.getint(MCP3008_SECTION, 'RIGHT_JOYSTICK_Y')
 
         # MCP23017
         self.START = gamepad_configs.getint(MCP23017_SECTION, 'START')
