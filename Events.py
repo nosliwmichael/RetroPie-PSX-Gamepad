9299d5919045c00a7e487eb61f3a92e1cfb7a935
nosliwmichael <nosliw.michael@gmail.com>
2024-02-01 02:13:38 -0600

    Try using a dictionary to pull event codes dynamically

--- Events.py
+++ Events.py
@@ -5,28 +5,29 @@ import RPi.GPIO as GPIO
 from MCP3008 import MCP3008
 
 # https://github.com/tuomasjjrasanen/python-uinput/blob/master/src/ev.py
-events = (
-    uinput.ABS_X + (0, 1023, 0, 0),   # Left joystick X-axis
-    uinput.ABS_Y + (0, 1023, 0, 0),   # Left joystick Y-axis
-    uinput.BTN_THUMBL,                # Left joystick button
-    uinput.ABS_RX + (0, 1023, 0, 0),  # Right joystick X-axis
-    uinput.ABS_RY + (0, 1023, 0, 0),  # Right joystick Y-axis
-    uinput.BTN_THUMBR,                # Right joystick button
-    uinput.BTN_SOUTH,
-    uinput.BTN_EAST,
-    uinput.BTN_WEST,
-    uinput.BTN_NORTH,
-    uinput.BTN_DPAD_UP,
-    uinput.BTN_DPAD_DOWN,
-    uinput.BTN_DPAD_LEFT,
-    uinput.BTN_DPAD_RIGHT,
-    uinput.BTN_START,
-    uinput.BTN_SELECT,
-    uinput.BTN_TL,
-    uinput.BTN_TR,
-    uinput.BTN_TL2,
-    uinput.BTN_TR2
-)
+event_map = {
+    "ABS_X" : uinput.ABS_X + (0, 1023, 0, 0),   # Left joystick X-axis
+    "ABS_Y": uinput.ABS_Y + (0, 1023, 0, 0),   # Left joystick Y-axis
+    "BTN_THUMBL": uinput.BTN_THUMBL,                # Left joystick button
+    "ABS_RX": uinput.ABS_RX + (0, 1023, 0, 0),  # Right joystick X-axis
+    "ABS_RY": uinput.ABS_RY + (0, 1023, 0, 0),  # Right joystick Y-axis
+    "BTN_THUMBR": uinput.BTN_THUMBR,                # Right joystick button
+    "BTN_SOUTH": uinput.BTN_SOUTH,
+    "BTN_EAST": uinput.BTN_EAST,
+    "BTN_WEST": uinput.BTN_WEST,
+    "BTN_NORTH": uinput.BTN_NORTH,
+    "BTN_DPAD_UP": uinput.BTN_DPAD_UP,
+    "BTN_DPAD_DOWN": uinput.BTN_DPAD_DOWN,
+    "BTN_DPAD_LEFT": uinput.BTN_DPAD_LEFT,
+    "BTN_DPAD_RIGHT": uinput.BTN_DPAD_RIGHT,
+    "BTN_START": uinput.BTN_START,
+    "BTN_SELECT": uinput.BTN_SELECT,
+    "BTN_TL": uinput.BTN_TL,
+    "BTN_TR": uinput.BTN_TR,
+    "BTN_TL2": uinput.BTN_TL2,
+    "BTN_TR": uinput.BTN_TR2
+}
+events = tuple(event_map.values())
 
 # The order of events handled must match the events tuple above
 def eventHandler(virtual_gamepad: uinput.Device, 
@@ -36,7 +37,7 @@ def eventHandler(virtual_gamepad: uinput.Device,
         input.value = readGpioPin(input)
         if (input.value != input.prev_value):
             printInput(input)
-            virtual_gamepad.emit(input.event_code, input.value, syn=False)
+            virtual_gamepad.emit(event_map[input.name], input.value, syn=False)
             input.prev_value = input.value
 
     for input in gamepad_map.mcp3008_inputs:
@@ -45,13 +46,13 @@ def eventHandler(virtual_gamepad: uinput.Device,
             (input.value != input.prev_value)
         ):
             printInput(input)
-            virtual_gamepad.emit(input.event_code, input.value, syn=False)
+            virtual_gamepad.emit(event_map[input.name], input.value, syn=False)
             input.prev_value = input.value
 
     # for input in gamepad_map.mcp23017_inputs:
     #     input.value = readGpioExpansionPin(input)
     #     if (input.value != input.prev_value):
-    #         virtual_gamepad.emit(input.event_code, input.value, syn=False)
+    #         virtual_gamepad.emit(event_map[input.name], input.value, syn=False)
     #         input.prev_value = input.value
     #         printInput(input)
     
