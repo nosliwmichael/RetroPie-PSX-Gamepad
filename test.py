6c71a79df74026e05f8720117cd2e7c944a2ad6d
nosliwmichael <nosliw.michael@gmail.com>
2024-02-01 02:00:36 -0600

    Try implementing weird tuples in gamepad config

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
-from GamepadMap import GamepadMap
+# from GamepadMap import GamepadMap
 
-for btn in GamepadMap().gpio_inputs:
-    print("name: {}, event_code: {}, channel: {}, is_digital: {}, value: {}, prev_value: {}"
-          .format(btn.name, btn.event_code, btn.channel, btn.is_digital, btn.value, btn.prev_value))
+# for btn in GamepadMap().gpio_inputs:
+#     print("name: {}, event_code: {}, channel: {}, is_digital: {}, value: {}, prev_value: {}"
+#           .format(btn.name, btn.event_code, btn.channel, btn.is_digital, btn.value, btn.prev_value))
     
-for btn in GamepadMap().mcp3008_inputs:
-    print("name: {}, event_code: {}, channel: {}, is_digital: {}, value: {}, prev_value: {}"
-          .format(btn.name, btn.event_code, btn.channel, btn.is_digital, btn.value, btn.prev_value))
+# for btn in GamepadMap().mcp3008_inputs:
+#     print("name: {}, event_code: {}, channel: {}, is_digital: {}, value: {}, prev_value: {}"
+#           .format(btn.name, btn.event_code, btn.channel, btn.is_digital, btn.value, btn.prev_value))
     
-for btn in GamepadMap().mcp23017_inputs:
-    print("name: {}, event_code: {}, channel: {}, is_digital: {}, value: {}, prev_value: {}"
-          .format(btn.name, btn.event_code, btn.channel, btn.is_digital, btn.value, btn.prev_value))
+# for btn in GamepadMap().mcp23017_inputs:
+#     print("name: {}, event_code: {}, channel: {}, is_digital: {}, value: {}, prev_value: {}"
+#           .format(btn.name, btn.event_code, btn.channel, btn.is_digital, btn.value, btn.prev_value))
+    
+
+events = (
+    0x00 + (0, 1023, 0, 0),
+    0x01 + (0, 1023, 0, 0),
+)
+
+print(events)
